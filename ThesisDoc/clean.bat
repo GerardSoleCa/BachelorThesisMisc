a8734908694c3c53f13087bf72a9c25d091910a9
Gerard <gerard@gsole.cat>
2013-10-19 19:43:54 +0200

    Initial Thesis Commit

---
+++ ThesisDoc/clean.bat
@@ -0,0 +1,14 @@
+@echo off
+DEL ".\*.log"
+DEL ".\*.lof"
+DEL ".\*.dvi"
+DEL ".\*.aux"
+DEL ".\*.lot"
+DEL ".\*.toc"
+DEL ".\*.ps"
+DEL ".\*.blg"
+DEL ".\*.bbl"
+DEL ".\*.out"
+
+DEL ".\chapters\*.aux"
+DEL ".\chapters\*.log"
